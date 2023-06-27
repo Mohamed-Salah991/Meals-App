3954c42732b82159cd16c112b12675e6b7bd4801
Mohamed Salah <mohamed.salah6486@gmail.com>
2023-06-27 03:29:49 +0300

    upload files

---
+++ src/components/UI/Input.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import classes from "./Input.module.css";
+
+const Input = React.forwardRef((props, ref) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input ref={ref} {...props.input} />
+    </div>
+  );
+});
+
+// {...props.input} => to add all key value to this input => like type, step, max, min, or any thing related to this input
+
+export default Input;
